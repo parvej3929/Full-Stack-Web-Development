9a7305ab6498e137fb2541db89870c73c77202ed
Parvej Saifi <parvejsaifi1311@gmail.com>
2026-04-18 20:33:48 +0530

    day-4

---
+++ fullstack-day4/homeWork-day4/script.js
@@ -0,0 +1,9 @@
+function changeText(){
+    let heading = document.getElementById("heading")
+    let text = document.getElementById("text")
+    heading.innerText = "Learning With Ai";
+    text.innerText = "Welcome to Ai powered e learning platform Parvej Saifi!";
+    heading.style.color= "blue";
+    text.style.color = "green";
+    text.style.fontSize = "20px"
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+function changeText(){
+    let element = document.getElementById("heading1")
+    element.innerText = "java dom is working!"
+    element.style.color =  green;
+    element.style.fontSize = "20px";
+}
