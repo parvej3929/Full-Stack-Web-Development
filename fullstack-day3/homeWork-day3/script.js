6437e9173252b90b3dad0d8dab8c42f2aec41f55
Parvej Saifi <parvejsaifi1311@gmail.com>
2026-04-18 18:40:59 +0530

    day -3 completed

---
+++ fullstack-day3/homeWork-day3/script.js
@@ -0,0 +1,3 @@
+function changePara(){
+    document.getElementById("demo").innerText = "This is a very tough time, continue and fight back, you'll definitly succed! "
+}
---
+++ fullstack-day3/script.js
@@ -0,0 +1,9 @@
+// alert("Welcome Parvej Saifi")
+
+function sayHello(){
+    alert("Thanks for hiring me!")
+}
+
+function changeText(){
+    document.getElementById('text').innerText = "Java Sxcript is awesome!"
+}
